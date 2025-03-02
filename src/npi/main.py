f296e1dfc0949cc8f0633d876732631fef540a9c
andrerhenry <kinghenry238@gmail.com>
2025-03-02 01:23:32 -0800

    started install parser

--- src/npi/main.py
+++ src/npi/main.py
@@ -1,5 +1,23 @@
 import os 
-import click
+import argparse
 
-print(os.name)
-click.Group()
+# Temp path to local dir for testing 
+global REPO_PATH
+
+def main():
+    parser = argparse.ArgumentParser(prog='npi', description='niagara package installer')
+    subparsers = parser.add_subparsers(help='subcommand help')
+
+    parser_install = subparsers.add_parser('install', help='list packages to be installed')
+    parser_install.add_argument('package')
+    args = parser.parse_args()
+    package = args.package
+    print(f"looking for {package}")
+    
+
+
+
+
+if __name__ == "__main__":
+    main()
+    
