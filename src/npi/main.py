53bff6ff291b7ec74d9846261d988a36821d661f
andrerhenry <kinghenry238@gmail.com>
2025-03-02 22:23:50 -0800

    reworked args in to subcommands

--- src/npi/main.py
+++ src/npi/main.py
@@ -1,30 +1,32 @@
 import os 
 import argparse
 
-from .list_modules import list
+from .list_modules import list_packages
 
 # Temp path to local dir for testing 
 global REPO_PATH
 
-def install(package):
-    print(f"looking for {package}")
+def install(args):
+    print(f"looking for {args.package}")
 
         
 
 def main():
     parser = argparse.ArgumentParser(prog='npi', description='niagara package installer')
-    subparsers = parser.add_subparsers(help='subcommand help')
+    subparsers = parser.add_subparsers(required=False, help='subcommand help')
 
     parser_install = subparsers.add_parser(name='install', help='list packages to be installed')
     parser_install.add_argument('package')
-    args_install = parser.parse_args()
-    package = args_install.package
-    install(package)
-    list()
+    parser_install.set_defaults(func=install)
 
     
-    parser.add_argument('-l', '--list', help='list current installed pakcages')
+    parser_list = subparsers.add_parser('list')
+    #parser.add_argument('list', help='list current installed pakcages')
+    parser_list.set_defaults(func=list_packages)
+    
     args = parser.parse_args()
+    args.func(args)
+
 
     
 
