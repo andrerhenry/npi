f5495770c6e87570e9f1e08fc7cfff4b9d0f0b95
andrerhenry <kinghenry238@gmail.com>
2025-03-02 21:02:12 -0800

    finished paths for search

--- src/npi/list_modules.py
+++ src/npi/list_modules.py
@@ -1,10 +1,14 @@
 import os
 from pathlib import Path
 
+def list() -> None:
+    base_path = Path(os.getcwd()) / "mock_install"
+    niagara_folder = "Niagara-4.14.0.162"
+    install_dir = base_path / niagara_folder / "modules"
 
-base_path = Path(os.getcwd()) / "mock_install"
-niagara_folder = "Niagara-4.14.0.162"
-install_dir = base_path / niagara_folder / "modules"
+    print("listing installed packages")
+    print(install_dir)
+    print(os.listdir(install_dir))
 
-print(install_dir)
-print(os.listdir(install_dir))
+if __name__ == "__main__":
+    list()
