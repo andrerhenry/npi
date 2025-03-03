ac0f567e6efa29a441c0bc1a6aaca8ebb2951db8
andrerhenry <kinghenry238@gmail.com>
2025-03-02 16:57:50 -0800

    fixed install fucntion call, moved definintion

--- src/npi/main.py
+++ src/npi/main.py
@@ -4,6 +4,12 @@ import argparse
 # Temp path to local dir for testing 
 global REPO_PATH
 
+def install(package):
+    print(f"looking for {package}")
+
+def list() -> None:
+        print("listing installed packages")
+
 def main():
     parser = argparse.ArgumentParser(prog='npi', description='niagara package installer')
     subparsers = parser.add_subparsers(help='subcommand help')
@@ -11,7 +17,8 @@ def main():
     parser_install = subparsers.add_parser(name='install', help='list packages to be installed')
     parser_install.add_argument('package')
     args_install = parser.parse_args()
-    package = args.package
+    package = args_install.package
+    install(package)
 
     
     parser.add_argument('-l', '--list', help='list current installed pakcages')
@@ -20,11 +27,7 @@ def main():
     
 
 
-    def install(package):
-        print(f"looking for {package}")
 
-    def list() -> None:
-         print("listing installed packages")
 
 
 
