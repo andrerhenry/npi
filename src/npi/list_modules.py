cc6fecbe9ec5c08e826af638bf20eaf55f2a7c25
andrerhenry <kinghenry238@gmail.com>
2025-03-03 16:05:00 -0800

    add printing loop for packages

--- src/npi/list_modules.py
+++ src/npi/list_modules.py
@@ -1,15 +1,18 @@
 import os
 from pathlib import Path
 
-def list_packages(args) -> None:
+def list_packages(*args ) -> None:
 
     base_path = Path(os.getcwd()) / "mock_install"
     niagara_folder = "Niagara-4.14.0.162"
     install_dir = base_path / niagara_folder / "modules"
 
-    print("listing installed packages")
+    print("Listing installed packages for  {install} at location:")
     print(install_dir)
-    print(os.listdir(install_dir))
+    package_list = os.listdir(install_dir)
+    
+    for package in package_list:
+        print(package)
 
 if __name__ == "__main__":
     list_packages()
