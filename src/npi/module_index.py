e9a7a385cffa8defdd0603c18e1351a4a8eb8f6d
andrerhenry <kinghenry238@gmail.com>
2025-03-04 23:13:26 -0800

    add rapidfuzz dependancy, modified search to find closest match if not exact

--- src/npi/module_index.py
+++ src/npi/module_index.py
@@ -2,6 +2,9 @@ import os
 from pathlib import Path
 from collections.abc import Mapping
 
+from rapidfuzz import fuzz, process
+
+
 def get_install_dir() -> Path:
     base_path = Path(os.getcwd()) / "mock_install"
     niagara_folder = "Niagara-4.14.0.162"
@@ -25,14 +28,19 @@ def list_modules(*args ) -> Mapping:
 def find_module(module_name: str) -> bool:
     install_dir = get_install_dir()
     module_list = os.listdir(install_dir)
-    
+
+    # Look in to droping file extention in index/search
+    search_results = process.extractOne(module_name, module_list, scorer=fuzz.ratio)
+    print(search_results[1])
     if module_name in module_list:
         print(f"Module: {module_name} found")
-    return  
+    elif search_results[1] >= 80:
+        print(f"Closet module is {search_results[0]}")
+    return  True
 
 
 if __name__ == "__main__":
     # Lines to test module 
     list_modules()
-    find_module("somemodule3.txt")
+    find_module("somemodule2")
 
