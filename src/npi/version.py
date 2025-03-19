c09128140efb8f4fd328145c90ce8e05dbcef637
andrerhenry <kinghenry238@gmail.com>
2025-03-19 00:24:16 -0700

    add niagara version func

--- src/npi/version.py
+++ src/npi/version.py
@@ -1,5 +1,6 @@
 import os 
 from pathlib import Path
+from collections import namedtuple
 
 
 def check_niagara_version():
@@ -8,8 +9,17 @@ def check_niagara_version():
 
     if parent_dir == 'bin' or parent_dir == 'modules':
         niagra_distro = (Path(os.getcwd()).parent).name
-    print(f'niagara distribution: {niagra_distro}')
+        print(f'niagara distribution: {niagra_distro}')
+        check_version(niagra_distro)
 
+
+
+def check_version(niagara_distro:str):
+    distributor = niagara_distro.split('-')[0]
+    niagara_version = niagara_distro.split('-')[1]
+    major_version = niagara_version.split('.')[0]
+    minor_version = niagara_version.split('.')[1]
+    print(distributor, niagara_version, major_version,minor_version)
 
 
 check_niagara_version()
