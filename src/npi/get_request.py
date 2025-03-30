d5da96064a53e9918572a0488eac93e2850d978a
andrerhenry <kinghenry238@gmail.com>
2025-03-30 01:34:42 -0700

    added help for sub command by description

--- src/npi/get_request.py
+++ src/npi/get_request.py
@@ -59,7 +59,10 @@ def add_install_parser(subparsers: _SubParsersAction) -> ArgumentParser:
     Returns:
         ArgumentParser: adds subarpse with added install actions
     """    
-    parser_install = subparsers.add_parser(name='install', help='Install specified package')
+    parser_install = subparsers.add_parser(
+        name='install', 
+        help='Install specified package', 
+        description='Install specified package')
     parser_install.add_argument('--niagara-version', type=str, metavar='', help='Override the version of niagara')
     parser_install.add_argument('package-name', type=str, help='Name of package to be installed')
     parser_install.set_defaults(func=install_package)
