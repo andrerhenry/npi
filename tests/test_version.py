68be4b27f88e0eee3ab2a54ef7dc4634bd5cd781
andrerhenry <kinghenry238@gmail.com>
2025-04-19 18:02:34 -0700

    added test for verison from properperteis file

--- tests/test_version.py
+++ tests/test_version.py
@@ -14,8 +14,6 @@ def sample_file(tmp_path) -> Path:
 
 @pytest.fixture
 def version_properties_file(tmp_path):
-    version_properties = tmp_path/'version.properties'
-    version_properties.mkdir()
     CONTENT = """releaseText1=Powered by Niagara 4 Framework
     releaseText2=Build 4.13.0.186
     releaseText3=Tue Jul 18 19:15:39 Coordinated Universal Time 2023
@@ -27,11 +25,10 @@ def version_properties_file(tmp_path):
     startMenuFolder=Niagara 4.13.0.186
     appName=wb.exe
     displayName=Niagara"""
+
+    version_properties = tmp_path/'version.properties'
     version_properties.write_text(CONTENT, encoding='utf-8')
 
-
-# def test_set_version_from_properties_file():
-#     version.set_version_from_properties_file()
 
 
 @pytest.mark.parametrize("folder_name", ["EC-Net4-4.13.0.186", "Niagara-4.14.0.162"])
@@ -44,4 +41,13 @@ def test_get_niagara_path(tmp_path, folder_name):
         version.get_niagara_version(tmp_path)
 
 
+def test_set_version_from_properties_file(tmp_path, version_properties_file):
+    poperties_object = version.read_version_properties_file(tmp_path)
+    version_object = version.set_version_from_properties_file(poperties_object)
+    
+    assert version_object.version == "4.13.0.186"
+    assert version_object.major_version == 4
+    assert version_object.minor_version == 13
+    assert version_object.iteration_version == 0
+    assert version_object.build_version == 186
 
