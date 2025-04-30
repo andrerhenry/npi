c7ac4abee6abd6af1effba0f10d544444332e656
andrerhenry <kinghenry238@gmail.com>
2025-04-29 22:19:09 -0700

    moved main parser to a build function to allow for easier testing

--- src/npi/main.py
+++ src/npi/main.py
@@ -6,29 +6,32 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.WARNING)
 logger.info('LoggingStarted')
 
-from .list import add_list_parsers
-from .search import add_search_parsers
-from .install import add_install_parser
-from .version import add_version_parser, npi_version
-from .errors import NiagaraSystemDetectionError, GetPackageManifestError
+from npi.list import add_list_parsers
+from npi.search import add_search_parsers
+from npi.install import add_install_parser
+from npi.version import add_version_parser, npi_version
+from npi.errors import NiagaraSystemDetectionError, GetPackageManifestError
 
 # Temp path to local dir for testing 
 global REPO_PATH
 
+def build_parser():
+    parser = ArgumentParser(prog='npi', 
+                        description='Niagara package installer helps you install and manage '
+                        'Niagara packages.')
+    parser.add_argument('--version', action='version', version=npi_version())
+
+    subparsers = parser.add_subparsers(title='subcommands', dest='subcommands', metavar='', required=False)
+    install_parser = add_install_parser(subparsers)
+    version_parser = add_version_parser(subparsers)
+    list_parser = add_list_parsers(subparsers)
+    search_parser = add_search_parsers(subparsers)
+    return parser
+
 
 def main():
+    parser = build_parser()
     try:
-        parser = ArgumentParser(prog='npi', 
-                                description='Niagara package installer helps you install and manage '
-                                'Niagara packages.')
-        parser.add_argument('--version', action='version', version=npi_version())
-
-        subparsers = parser.add_subparsers(title='subcommands', dest='subcommands', metavar='', required=False)
-        install_parser = add_install_parser(subparsers)
-        version_parser = add_version_parser(subparsers)
-        list_parser = add_list_parsers(subparsers)
-        search_parser = add_search_parsers(subparsers)
-
         args = parser.parse_args()
         args.func(args)
     except NiagaraSystemDetectionError as error:
